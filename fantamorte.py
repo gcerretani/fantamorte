a13cf50398d785b646a4f63c2791890b23922417
Giovanni Cerretani <giovanni.cerretani@gmail.com>
2019-05-25 12:30:02 +0200

    file added

---
+++ fantamorte.py
@@ -0,0 +1,79 @@
+import wptools
+import datetime
+import dateutil.parser
+
+people = {}
+
+def get_data(myname):
+    if myname in people:
+        return people[myname]
+    else: 
+        pdata = {}
+        p = wptools.page(myname, lang='it', silent=True)
+        p.wanted_labels([])
+        page = p.get_wikidata()
+#         for item in page.data:
+#             print(item , page.data[item])
+
+        name = page.data['label']
+        descr = page.data['description']
+        claims = page.data['claims']
+        birth = None
+        death = None
+        nobel = False
+        alive = False
+
+        if 'P569' in claims:
+            d = claims['P569'][0][1:]
+            birth = dateutil.parser.parse(d)
+        else:
+            birth = None
+            print("no birth date")
+
+        if 'P570' in claims:
+            d = claims['P570'][0][1:]
+            death = dateutil.parser.parse(d)
+        else:
+            alive = True
+            death = None
+
+        if 'P3188' in claims:
+            nobel = True
+        else:
+            nobel = False
+
+#         if 'P166' in claims:
+#             for i in claims['P166']:
+#                 print(i)
+
+        age = calculate_age(birth,death)
+
+        pdata['name'] = name
+        pdata['descr'] = descr
+        pdata['birth'] = birth
+        pdata['death'] = death
+        pdata['age'] = age
+        pdata['alive'] = alive
+        pdata['nobel'] = nobel
+
+        people[myname] = pdata
+
+        return pdata
+
+def calculate_age(birth, death = None):
+    if death:
+        today = death
+    else:
+        today = datetime.date.today()
+    return today.year - birth.year - ((today.month, today.day) < (birth.month, birth.day))
+
+
+with open('names.txt') as names:
+    for myname in names:
+        myname = myname.strip('\n')
+
+        data = get_data(myname)
+
+        if not data['alive']:
+            print(data['name'], data['age'], data ['alive'], data['nobel'])
+
