a2c0f87f1c2ac2d7e31dbbc271655c9582730a12
Giovanni Cerretani <cerrigno@gmail.com>
2026-09-13 19:44:25 +0200

    Release: set version to 1.0.0

--- fantamorte_project/version.py
+++ fantamorte_project/version.py
@@ -1,7 +1,3 @@
-"""Fantamorte application version.
+"""Fantamorte application version."""
 
-Keep the unreleased development suffix on develop; release preparation removes
-it when the corresponding tag is created.
-"""
-
-__version__ = '1.0.0-dev'
+__version__ = '1.0.0'
