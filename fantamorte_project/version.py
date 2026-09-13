e830864d05e51c804cb1a22dad8e804f94b2e040
Giovanni Cerretani <cerrigno@gmail.com>
2026-09-13 07:25:40 +0200

    Add single application version source

---
+++ fantamorte_project/version.py
@@ -0,0 +1,7 @@
+"""Fantamorte application version.
+
+Keep the unreleased development suffix on develop; release preparation removes
+it when the corresponding tag is created.
+"""
+
+__version__ = '1.0.0-dev'
